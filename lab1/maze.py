511c5ea22ff43b0520cbe5860681ddd1829e8564
Leo Tiez <leozeitler95@gmail.com>
2019-11-13 12:49:00 +0100

    Update: Implemented maze class with constructor
    Maze class is supposed to be resposible for creating the maze
    and tracking/changing the state

--- lab1/maze.py
+++ lab1/maze.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 import numpy as np
 
+
+class Maze:
+    def __init__(self, size=(7, 8), init_pos_a=(0, 0), init_pos_b=(6,5), wall_mask=None):
+        self.maze_size = size
+        self.pos_a = init_pos_a
+        self.pos_b = init_pos_b
+        
