c3d1efadc6fec32955b992fe695e667b37d46d9a
Leo Tiez <leozeitler95@gmail.com>
2019-11-13 18:29:24 +0100

    Bugfix: Fixed indexing problems in next state function
    Due to indexing problems and index updates the data structure
    needed to be updated from tuples to arrays. Moreover, the collapsing
    of the map to only non-wall cells and then updating the state of B
    for efficiency reasons did not work. Thus a loop is used now instead

--- lab1/maze.py
+++ lab1/maze.py
@@ -11,11 +11,11 @@ class Maze:
     }
 
     MOVE_DICT = {
-        'up': (1, 0),
-        'down': (-1, 0),
-        'right': (0, 1),
-        'left': (0, -1),
-        'stay': (0, 0)
+        'up': np.asarray([-1, 0]),
+        'down': np.asarray([1, 0]),
+        'right': np.asarray([0, 1]),
+        'left': np.asarray([0, -1]),
+        'stay': np.asarray([0, 0])
     }
 
     MOVE_ARRAY = ['up', 'down', 'right', 'left', 'stay']
@@ -23,22 +23,23 @@ class Maze:
     A = 1
     B = 2
 
-    def __init__(self, size=(7, 8), init_pos_a=(0, 0), init_pos_b=(6, 5), goal_state=(6, 5), wall_mask=None):
+    def __init__(self, size=(7, 8), init_pos_a=[0, 0], init_pos_b=[6, 5], goal_state=[6, 5], wall_mask=None):
         self.maze_size = size
-        self.pos_a = init_pos_a
-        self.pos_b = init_pos_b
-        self.goal_state = goal_state
+        self.pos_a = np.asarray(init_pos_a)
+        self.pos_b = np.asarray(init_pos_b)
+        self.goal_state = np.asarray(goal_state)
         self.wall_mask = wall_mask
         self.maze = self._create_maze()
 
     def next_state(self, move):
         # Calculate move of a
         next_a = self.pos_a + Maze.MOVE_DICT[move]
-        out_of_left_bound = next_a[1] < 0
-        out_of_right_bound = next_a[1] >= self.maze_size[1]
-        out_of_upper_bound = next_a[0] < 0
-        out_of_lower_bound = next_a[0] >= self.maze_size[0]
-        is_wall = np.isneginf(self.maze[next_a])
+
+        (out_of_left_bound,
+         out_of_right_bound,
+         out_of_upper_bound,
+         out_of_lower_bound) = self._is_in_bounds(next_a)
+        is_wall = np.isneginf(self.maze[next_a[0], next_a[1]])
 
         if out_of_left_bound or out_of_right_bound or out_of_lower_bound or out_of_upper_bound or is_wall:
             next_a = self.pos_a
@@ -47,43 +48,58 @@ class Maze:
         move_b = Maze.MOVE_DICT[np.random.choice(Maze.MOVE_ARRAY)]
         next_b = self.pos_b + move_b
 
-        out_of_left_bound = next_b[1] < 0
-        out_of_right_bound = next_b[1] >= self.maze_size[1]
-        out_of_upper_bound = next_b[0] < 0
-        out_of_lower_bound = next_b[0] >= self.maze_size[0]
+        (out_of_left_bound,
+         out_of_right_bound,
+         out_of_upper_bound,
+         out_of_lower_bound) = self._is_in_bounds(next_b)
         if out_of_left_bound or out_of_right_bound or out_of_lower_bound or out_of_upper_bound:
-            move_b = Maze.MOVE_DICT['stay']
+            next_b = self.pos_b
 
         # Set move of b
-        # Find move of b in a maze without walls
-        next_b_in_maze_wout_walls = np.where(self.maze[~self.wall_mask] == Maze.B) + move_b
-        # Reset previous position
-        self.maze[self.pos_b] = 0
-        # Set new position via collapsing maze
-        self.maze[~self.wall_mask][next_b_in_maze_wout_walls] = Maze.B
-        # Find new position
-        next_b = np.where(self.maze == Maze.B)
+        while np.isneginf(self.maze[next_b[0], next_b[1]]):
+            next_b += move_b
+            (out_of_left_bound,
+             out_of_right_bound,
+             out_of_upper_bound,
+             out_of_lower_bound) = self._is_in_bounds(next_b)
+            if out_of_left_bound or out_of_right_bound or out_of_lower_bound or out_of_upper_bound:
+                next_b = self.pos_b
+                break
 
-        if next_b == next_a:
+
+        # Reset previous position
+        self.maze[self.pos_b[0], self.pos_b[1]] = 0
+        # Set new position via collapsing maze
+        self.maze[next_b[0], next_b[1]] = Maze.B
+
+        if np.all(next_b == next_a):
             return Maze.STATE_DICT['loosing']
 
         # Set new positions
+        self.maze[self.pos_a[0], self.pos_a[1]] = 0
+        self.maze[next_a[0], next_a[1]] = Maze.A
         self.pos_a = next_a
-        self.maze[self.pos_a] = Maze.A
         self.pos_b = next_b
 
-        if next_a == self.goal_state:
+        if np.all(next_a == self.goal_state):
             return Maze.STATE_DICT['winning']
         else:
             return Maze.STATE_DICT['running']
+
+    def _is_in_bounds(self, state_indices):
+        out_of_left_bound = state_indices[1] < 0
+        out_of_right_bound = state_indices[1] >= self.maze_size[1]
+        out_of_upper_bound = state_indices[0] < 0
+        out_of_lower_bound = state_indices[0] >= self.maze_size[0]
+        return  out_of_left_bound, out_of_right_bound, out_of_upper_bound, out_of_lower_bound
 
     def _create_maze(self):
         if self.wall_mask is None:
             self.wall_mask = self._create_default_wall_mask()
         maze = np.zeros(self.maze_size)
         maze[self.wall_mask] = np.NINF
-        maze[self.pos_a] = Maze.A
-        maze[self.pos_b] = Maze.B
+        maze[self.pos_a[0], self.pos_a[1]] = Maze.A
+        maze[self.pos_b[0], self.pos_b[1]] = Maze.B
 
         return maze
 
@@ -101,5 +117,8 @@ class Maze:
 
 
 if __name__ == '__main__':
+    np.random.seed(1)
     maze = Maze()
+    maze.next_state('down')
+
 
